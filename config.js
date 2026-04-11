27152ef4575b504d17abfca8574e1fc40ecf62e0
decko28 <146557342+decko28@users.noreply.github.com>
2026-04-11 22:36:10 +0800

    Create config.js

---
+++ config.js
@@ -0,0 +1,6 @@
+// config.js
+const CONFIG = {
+  SUPABASE_URL: 'https://bzbdfxyjiainooxoopfq.supabase.co',
+  SUPABASE_ANON_KEY: 'sb_publishable_oj9lc25LX7qYmrRGLpNt2A_2ISYheVC',
+  GAS_API: 'https://script.google.com/macros/s/AKfycbz79Ba5eQcp12kJ1_4DtkCd6_H4HZQcvTbmjEv0jykcosSoBzLE9P5xY6A_9qWg3x73rg/exec'
+};
