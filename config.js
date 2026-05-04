3c131a1d312a61ce5331d0f94a5219769d6a2897
decko28 <146557342+decko28@users.noreply.github.com>
2026-05-04 14:09:08 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
   SUPABASE_URL: 'https://bzbdfxyjiainooxoopfq.supabase.co',
   SUPABASE_ANON_KEY: 'sb_publishable_oj9lc25LX7qYmrRGLpNt2A_2ISYheVC',
-  GAS_API: 'https://script.google.com/macros/s/AKfycbzFJx6zCeuO4Axz6AI88dFzrGmxGZkRq7wldjwv3yaz8PQ4C5OSJI2WOSCejfZ4O8M-jA/exec'
+  GAS_API: 'https://script.google.com/macros/s/AKfycbw_uvJ44ovj7XRoP7I0m-dz6eoApHYo36zi2HjAyYtjkuYzBZPY6op0OqAasNkvF4mB2g/exec'
 };
